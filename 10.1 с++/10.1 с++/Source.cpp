0f5581293d2dc8af822bcc3ec952bf4ffe55b5e7
BohdanKalymon <72624509+BohdanKalymon@users.noreply.github.com>
2021-01-12 02:39:53 +0200

    Add files via upload

---
+++ 10.1 с++/10.1 с++/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int Count();
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Кількість ,- t.txt:" << Count() << endl;
+    return 0;
+}
+int Count()
+{
+    ifstream t("t.txt");
+    string s;
+
+    int k = 0;
+
+    while (getline(t, s))
+    {
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s[i] == ',' && s[i + 1] == '-')
+                k++;
+        }
+    }
+    return k;
+}
---
+++ 10.1 с++/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <search.h>
+#include "../10.1 с++/Source.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			// egr,- ,- ,- ,- ,- ,- ,-reee //
+			Assert::AreEqual(Count(), 7);
+		}
+	};
+}
